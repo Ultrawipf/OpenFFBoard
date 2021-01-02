958152e78f66a34b069905f5027aa31dc8c24287
Yannick Richter <git@y-richter.de>
2021-01-02 21:39:55 +0100

    Set SPI2 to msb first default in cubemx

--- Firmware/Targets/F411RE/Core/Src/main.c
+++ Firmware/Targets/F411RE/Core/Src/main.c
@@ -494,7 +494,7 @@ static void MX_SPI2_Init(void)
   hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi2.Init.NSS = SPI_NSS_SOFT;
   hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
-  hspi2.Init.FirstBit = SPI_FIRSTBIT_LSB;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
   hspi2.Init.CRCPolynomial = 10;
