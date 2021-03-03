e8c02e3330b660876c789bd14d576b7b319fe9f6
Yannick Richter <git@y-richter.de>
2021-03-03 16:19:30 +0100

    more led blink effects for feedback

--- Firmware/FFBoard/Inc/ledEffects.h
+++ Firmware/FFBoard/Inc/ledEffects.h
@@ -7,11 +7,27 @@
 
 #ifndef LEDEFFECTS_H_
 #define LEDEFFECTS_H_
+#include "main.h"
+typedef struct Ledstruct{
+	uint32_t tick;
+	uint16_t period;
+	int32_t blinks;
+	GPIO_TypeDef* port;
+	uint16_t pin;
+} Ledstruct_t;
 
 
+void blinkLed(Ledstruct_t* led,uint16_t period,uint16_t blinks);
 void pulseSysLed();
 void pulseErrLed();
 void pulseClipLed();
+
+void blinkSysLed(uint16_t period,uint16_t blinks);
+void blinkErrLed(uint16_t period,uint16_t blinks);
+void blinkClipLed(uint16_t period,uint16_t blinks);
+
+void updateLed(Ledstruct_t* led);
 void updateLeds();
+
 
 #endif /* LEDEFFECTS_H_ */
