1c86f42de4edf72c568025245ed164c4b23633b5
Jon Lidgard <jonlidgard@gmail.com>
2021-03-21 16:34:17 +0900

    Non-Condition forces now correctly calculated once per update.

--- Firmware/FFBoard/Inc/EffectsCalculator.h
+++ Firmware/FFBoard/Inc/EffectsCalculator.h
@@ -60,8 +60,9 @@ private:
 	const float cfFilter_q = 0.8;
 
 
-	int32_t calculateForce(FFB_Effect* effect, metric_t* metrics, effect_gain_t* gain, uint8_t axis, uint8_t axisCount);
-	int32_t calcCondition(FFB_Effect *effect, float metric, uint8_t gain, bool useDir,
+	int32_t calcComponentForce(FFB_Effect* effect, int32_t forceVector, metric_t* metrics, effect_gain_t* gain, uint8_t axis, uint8_t axisCount);
+	int32_t calcNonConditionEffectForce(FFB_Effect* effect);
+	int32_t calcConditionEffectForce(FFB_Effect *effect, float metric, uint8_t gain, bool useDir,
 			uint8_t idx, float scale, float angle_ratio);
 	int32_t applyEnvelope(FFB_Effect *effect, int32_t value);
 };
