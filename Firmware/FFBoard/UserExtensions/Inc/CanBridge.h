5b40df45774e29ab6e66af0e3eee3a99dd6b71b8
Yannick Richter <git@y-richter.de>
2021-06-20 19:12:03 +0200

    can bridge receive transfer not in interrupt

--- Firmware/FFBoard/UserExtensions/Inc/CanBridge.h
+++ Firmware/FFBoard/UserExtensions/Inc/CanBridge.h
@@ -26,6 +26,18 @@ typedef struct{
 	bool enabled = false;
 } CAN_Config_t;
 
+typedef struct{
+	uint8_t data[8];
+	CAN_RxHeaderTypeDef header;
+} CAN_rx_msg;
+
+typedef struct{
+	uint8_t data[8];
+	CAN_TxHeaderTypeDef header;
+} CAN_tx_msg;
+
+
+
 /*
  * A main class for testing CAN communication.
  * Sends CAN messages via CDC and can transmit CAN messages with 4 bytes
@@ -40,28 +52,32 @@ public:
 	const ClassIdentifier getInfo() override;
 	static bool isCreatable() {return true;};
 	ParseStatus command(ParsedCommand* cmd,std::string* reply);
-	void sendMessage(uint32_t id, uint64_t msg,uint8_t len);
+	void sendMessage(uint32_t id, uint64_t msg,uint8_t len,bool rtr);
 	void sendMessage();
 	void canRxPendCallback(CAN_HandleTypeDef *hcan,uint8_t* rxBuf,CAN_RxHeaderTypeDef* rxHeader,uint32_t fifo);
 	void canErrorCallback(CAN_HandleTypeDef *hcan);
-	std::string messageToString();
+	std::string messageToString(CAN_rx_msg msg);
 	void cdcRcv(char* Buf, uint32_t *Len) override;
 	CAN_HandleTypeDef* CanHandle = &CANPORT;
 
 	void setCanSpeed(uint32_t speed);
+	void update(); // Main loop
+	volatile bool replyPending = false;
 
-	virtual std::string getHelpstring(){return "CAN commands:\ncan?[id]=[msgint] send message. Or can? (last received message). canspd (speed).\nThis class is GVRET/SavvyCAN compatible!";}
+	virtual std::string getHelpstring(){return "CAN commands:\ncan?[id]=[msgint] send message (canrtr with rtr bit). Or can? (last received message). canspd (speed).\nThis class is GVRET/SavvyCAN compatible!";}
 
 
 private:
+	std::vector<CAN_rx_msg> rxmessages;
+	CAN_rx_msg lastmsg;
 	int32_t filterId = -1;
 	const uint8_t numBuses = 1;
 	uint32_t speed = 500000; // default
 
 	CAN_TxHeaderTypeDef txHeader;
-	CAN_RxHeaderTypeDef rxHeader; // Receive header
+	//CAN_RxHeaderTypeDef rxHeader; // Receive header
 
-	uint8_t rxBuf[8] = {0};
+	//uint8_t rxBuf[8] = {0};
 	uint8_t txBuf[8] = {0};
 	uint32_t txMailbox;
 	uint32_t rxfifo = CAN_RX_FIFO0;
