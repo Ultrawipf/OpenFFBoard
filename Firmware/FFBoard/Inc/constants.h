7c8ff514b04de0b087213100b3a5ccc86fbdb9af
Yannick Richter <git@y-richter.de>
2022-01-05 12:47:24 +0100

    Version bump 1.6.1

--- Firmware/FFBoard/Inc/constants.h
+++ Firmware/FFBoard/Inc/constants.h
@@ -10,7 +10,7 @@
 
 
 
-static const uint8_t SW_VERSION_INT[3] = {1,6,0}; // Version as array. 8 bit each!
+static const uint8_t SW_VERSION_INT[3] = {1,6,1}; // Version as array. 8 bit each!
 
 #define MAX_AXIS 2 // ONLY USE 2 for now else screws HID Reports
 
