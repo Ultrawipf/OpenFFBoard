ee3e7981275e6917114e6ad52c5ece161c0c4e5f
Yannick Richter <git@y-richter.de>
2022-03-09 17:29:17 +0100

    Axis setting ext encoder feature

--- Firmware/FFBoard/Inc/Axis.h
+++ Firmware/FFBoard/Inc/Axis.h
@@ -224,7 +224,9 @@ private:
 	uint16_t power = 2000;
 	float torqueScaler = 0; // power * fx_ratio as a ratio between 0 & 1
 	bool invertAxis = false;
-	uint8_t endstop_gain = 127; // Sets how much extra torque per count above endstop is added. High = stiff endstop. Low = softer
+	uint8_t endstopStrength = 127; // Sets how much extra torque per count above endstop is added. High = stiff endstop. Low = softer
+	const float endstopGain = 0.75; // Overall max endstop intensity
+
 
 	uint8_t idlespringstrength = 127;
 	int16_t idlespringclip = 0;
