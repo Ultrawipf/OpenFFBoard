a51f36f7fa9416dcac071ce9aaffba9b1f02ddde
Yannick Richter <git@y-richter.de>
2022-04-07 15:12:40 +0200

    Added can buttons

---
+++ Firmware/FFBoard/UserExtensions/Inc/CanButtons.h
@@ -0,0 +1,51 @@
+/*
+ * CanButtons.h
+ *
+ *  Created on: 06.04.2022
+ *      Author: Yannick
+ */
+
+#ifndef USEREXTENSIONS_SRC_CANBUTTONS_H_
+#define USEREXTENSIONS_SRC_CANBUTTONS_H_
+
+#include "constants.h"
+#ifdef CANBUTTONS
+
+#include "ButtonSource.h"
+#include "PersistentStorage.h"
+#include "CAN.h"
+#include "CommandHandler.h"
+
+class CanButtons : public ButtonSource, public PersistentStorage, public CanHandler, public CommandHandler {
+public:
+	enum class CanButtons_commands : uint32_t {
+		btnnum,invert,canid
+	};
+
+	CanButtons();
+	virtual ~CanButtons();
+
+	void restoreFlash();
+	void saveFlash();
+
+	uint8_t readButtons(uint64_t* buf);
+	void setupCanPort();
+	void setBtnNum(uint8_t num);
+
+	CommandStatus command(const ParsedCommand& cmd,std::vector<CommandReply>& replies);
+
+	void canRxPendCallback(CAN_HandleTypeDef *hcan,uint8_t* rxBuf,CAN_RxHeaderTypeDef* rxHeader,uint32_t fifo);
+
+private:
+	bool invert = false;
+	uint32_t canId = 25;
+	int32_t filterId = -1;
+	CANPort* port = &canport;
+	uint64_t mask = 0xff;
+
+	uint64_t currentButtons = 0;
+};
+
+
+#endif
+#endif /* USEREXTENSIONS_SRC_CANBUTTONS_H_ */
