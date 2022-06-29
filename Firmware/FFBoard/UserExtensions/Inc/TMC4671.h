50c05edcb73e36b8a6b6b82404f4041709ccebdd
Yannick Richter <git@y-richter.de>
2022-06-29 10:28:13 +0200

    Basic f407 disco build

--- Firmware/FFBoard/UserExtensions/Inc/TMC4671.h
+++ Firmware/FFBoard/UserExtensions/Inc/TMC4671.h
@@ -8,6 +8,7 @@
 #ifndef TMC4671_H_
 #define TMC4671_H_
 #include "constants.h"
+#ifdef TMC4671DRIVER
 #include <vector>
 #include "cppmain.h"
 #include "MotorDriver.h"
@@ -640,7 +641,7 @@ public:
 	static bool isCreatable();
 	static ClassIdentifier info;
 };
-
+#endif
 #endif /* TMC4671_H_ */
 
 
