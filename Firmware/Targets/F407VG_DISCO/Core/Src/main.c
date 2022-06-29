d154acd05bcf4908382c4b47fa04e9ede84b6268
Yannick Richter <git@y-richter.de>
2022-06-29 20:39:39 +0200

    Fixed encoder timer mode

--- Firmware/Targets/F407VG_DISCO/Core/Src/main.c
+++ Firmware/Targets/F407VG_DISCO/Core/Src/main.c
@@ -754,7 +754,7 @@ static void MX_TIM2_Init(void)
   htim2.Init.Period = 0xffffffff;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  sConfig.EncoderMode = TIM_ENCODERMODE_TI1;
+  sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
   sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
   sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
