b391c2fecf7151c7b25a50df559457e816b366b2
Yannick Richter <git@y-richter.de>
2022-08-19 13:37:51 +0200

    Added TMC4671 check to axis for disco

--- Firmware/FFBoard/Src/Axis.cpp
+++ Firmware/FFBoard/Src/Axis.cpp
@@ -7,7 +7,7 @@
 
 #include "Axis.h"
 #include "voltagesense.h"
-
+#include "TMC4671.h"
 
 //////////////////////////////////////////////
 /*
@@ -808,11 +808,13 @@ CommandStatus Axis::command(const ParsedCommand& cmd,std::vector<CommandReply>& 
 		if (cmd.type == CMDtype::get && this->drv->getEncoder() != nullptr)
 		{
 			uint32_t cpr = this->drv->getEncoder()->getCpr();
+#ifdef TMC4671
 			TMC4671 *tmcdrv = dynamic_cast<TMC4671 *>(this->drv.get()); // Special case for TMC. Get the actual encoder resolution
 			if (tmcdrv)
 			{
 				cpr = tmcdrv->getEncCpr();
 			}
+#endif
 			replies.emplace_back(cpr);
 		}else{
 			return CommandStatus::ERR;
