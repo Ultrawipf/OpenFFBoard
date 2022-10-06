80da7b82de8395cccdf80cfedf6f573ac182985d
Yannick Richter <Ultrawipf@users.noreply.github.com>
2022-10-06 12:29:31 +0200

    Support for local encoder index (#81)
    * Support for local encoder index
    
    * Changed Z interrupt mode to falling edge

--- Firmware/FFBoard/UserExtensions/Inc/EncoderLocal.h
+++ Firmware/FFBoard/UserExtensions/Inc/EncoderLocal.h
@@ -22,7 +22,7 @@ extern TIM_HandleTypeDef TIM_ENC;
 
 class EncoderLocal: public Encoder,public ExtiHandler,TimerHandler,public CommandHandler, public PersistentStorage{
 	enum class EncoderLocal_commands : uint32_t{
-		cpr
+		cpr,useindex
 	};
 public:
 	static bool inUse;
@@ -39,24 +39,30 @@ public:
 
 	int32_t getPos();
 	void setPos(int32_t pos);
-	void setOffset(int32_t offset);
+	//void setOffset(int32_t offset);
 	void setPeriod(uint32_t period);
 	void overflowCallback();
-	void exti(uint16_t GPIO_Pin);
+	void exti(uint16_t GPIO_Pin) override;
 	void timerElapsed(TIM_HandleTypeDef* htim);
+
+	int32_t getTimerCount();
 
 	void setCpr(uint32_t cpr);	// Encoder counts per rotation
 
 	CommandStatus command(const ParsedCommand& cmd,std::vector<CommandReply>& replies);
-	std::string getHelpstring(){return "Local encoder: cpr\n";}
+	std::string getHelpstring(){return "Local ABN encoder\n";}
 	void saveFlash(); 		// Write to flash here
 	void restoreFlash();	// Load from flash
 
 	//const ClassType getClassType() override {return ClassType::Encoder;};
 private:
 	TIM_HandleTypeDef* htim;
-	int32_t offset = 0;
+	int16_t offset = 0;
 	int32_t pos = 0; // Extra position counter for overflows
+	uint32_t indexpos = 0;
+
+	bool indexHit = false;
+	bool useIndex = false;
 };
 
 #endif /* ENCODERLOCAL_H_ */
--- Firmware/FFBoard/UserExtensions/Src/EncoderLocal.cpp
+++ Firmware/FFBoard/UserExtensions/Src/EncoderLocal.cpp
@@ -21,15 +21,21 @@ EncoderLocal::EncoderLocal() : CommandHandler("localenc",CLSID_ENCODER_LOCAL) {
 	EncoderLocal::inUse = true;
 	this->restoreFlash();
 	this->htim = &TIM_ENC;
+	HAL_TIM_Base_Start_IT(htim); // May immediately call overflow. Initialize count again
+	this->htim->Instance->CNT = 0x7fff;
+	pos = 0;
 	this->htim->Instance->CR1 = 1;
-	HAL_TIM_Base_Start_IT(htim);
-	setPos(0);
+
+	if(!useIndex)
+		setPos(0);
+
 	registerCommands();
 }
 
 void EncoderLocal::registerCommands(){
 	CommandHandler::registerCommands();
 	registerCommand("cpr", EncoderLocal_commands::cpr, "CPR of encoder",CMDFLAG_GET|CMDFLAG_SET);
+	registerCommand("index", EncoderLocal_commands::useindex, "Enable index pin",CMDFLAG_GET|CMDFLAG_SET);
 }
 
 EncoderLocal::~EncoderLocal() {
@@ -40,6 +46,8 @@ EncoderLocal::~EncoderLocal() {
 
 void EncoderLocal::saveFlash(){
 	Flash_Write(ADR_ENCLOCAL_CPR, this->getCpr());
+	Flash_Write(ADR_ENCLOCAL_OFS, useIndex ? (int16_t)this->offset : 0); // 0 offset indicates index off
+
 }
 
 void EncoderLocal::restoreFlash(){
@@ -47,6 +55,16 @@ void EncoderLocal::restoreFlash(){
 	if (Flash_Read(ADR_ENCLOCAL_CPR, &cpr)){
 		this->setCpr(cpr);
 	}
+	uint16_t ofs = 0;
+	if(Flash_Read(ADR_ENCLOCAL_OFS, &ofs)){
+		this->offset = (int16_t)ofs;
+		useIndex = ofs != 0;
+	}
+
+}
+
+int32_t EncoderLocal::getTimerCount(){
+	return (int32_t)htim->Instance->CNT - (int32_t)0x7fff;
 }
 
 EncoderType EncoderLocal::getEncoderType(){
@@ -55,14 +73,22 @@ EncoderType EncoderLocal::getEncoderType(){
 
 
 int32_t EncoderLocal::getPos(){
-	int32_t timpos = htim->Instance->CNT - (int32_t)0x7fff;
-	return timpos + pos;
-}
-void EncoderLocal::setPos(int32_t pos){
-	this->pos = pos;
-	htim->Instance->CNT = pos+0x7fff;
+	return getTimerCount() + pos;
 }
 
+void EncoderLocal::setPos(int32_t pos){
+	int32_t cnt = getTimerCount();
+
+	this->pos = pos - cnt;
+	if(useIndex && indexHit){
+		int32_t idxpos = ((int32_t)indexpos - (int32_t)0x7fff);
+		offset = ( ( idxpos-cnt-pos ) % this->cpr); // offset is distance to index position (0 = 0x7fff index)
+	}
+	//this->pos = pos - cnt;
+	//htim->Instance->CNT = 0x7fff; // Reset //pos+0x7fff;
+
+
+}
 
 
 void EncoderLocal::setPeriod(uint32_t period){
@@ -71,7 +97,18 @@ void EncoderLocal::setPeriod(uint32_t period){
 
 void EncoderLocal::exti(uint16_t GPIO_Pin){
 	if(GPIO_Pin == ENCODER_Z_Pin){
+		if(HAL_GPIO_ReadPin(ENCODER_Z_GPIO_Port, ENCODER_Z_Pin) == GPIO_PIN_RESET){
 		// Encoder Z pin activated
+
+			if(useIndex && !indexHit){
+				pulseSysLed();
+				htim->Instance->CNT = 0x7fff; // Center encoder
+				this->pos = offset;
+			}
+			indexpos = htim->Instance->CNT; // Store current encoder count
+			indexHit = true;
+		}
+
 	}
 }
 
@@ -98,19 +135,12 @@ CommandStatus EncoderLocal::command(const ParsedCommand& cmd,std::vector<Command
 	case EncoderLocal_commands::cpr:
 		handleGetFuncSetFunc(cmd, replies, &EncoderLocal::getCpr,&EncoderLocal::setCpr,this);
 	break;
+	case EncoderLocal_commands::useindex:
+		return handleGetSet(cmd, replies, useIndex);
+	break;
 	default:
 		return CommandStatus::NOT_FOUND;
 	}
 
 	return CommandStatus::OK;
-//	if(cmd->cmd == "cpr"){
-//		if(cmd->type == CMDtype::get){
-//			*reply += std::to_string(this->getCpr());
-//		}else if(cmd->type == CMDtype::set){
-//			this->setCpr(cmd->val);
-//		}
-//	}else{
-//		return ParseStatus::NOT_FOUND;
-//	}
-//	return ParseStatus::OK;
 }
