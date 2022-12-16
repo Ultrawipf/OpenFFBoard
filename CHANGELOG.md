28ca66e6e549853405f779823b4ae204a61efe21
Yannick Richter <git@y-richter.de>
2022-12-16 11:34:51 +0100

    Added SPI button speed option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,4 @@
 - Added Serial FFB mainclass mode
 - Added mosfet BBM time to TMC hardware selection
 - TMC enable pin is set when TMC debug mode starts
+- Added option to change SPI speed for buttons. Can be helpful if the connection is unreliable
--- Firmware/FFBoard/UserExtensions/Inc/SPIButtons.h
+++ Firmware/FFBoard/UserExtensions/Inc/SPIButtons.h
@@ -28,17 +28,19 @@ struct ButtonSourceConfig{
 	bool invert = false;
 	SPI_BtnMode mode=SPI_BtnMode::TM; // Mode preset
 	uint8_t cs_num = 0;
+	uint8_t spi_speed = 1; // Medium
 };
 
 
 class SPI_Buttons: public ButtonSource,public CommandHandler,public SPIDevice {
 
 	enum class SPIButtons_commands : uint32_t {
-		mode,btncut,btnpol,btnnum,cs
+		mode,btncut,btnpol,btnnum,cs,spispeed
 	};
 
 public:
-	const std::vector<std::string> mode_names = {"Thrustmaster/HEF4021BT","Shift register (74HC165)"};
+	static const std::vector<std::string> mode_names;
+	static const std::vector<std::string> speed_names;
 
 	virtual ~SPI_Buttons();
 
@@ -52,11 +54,13 @@ public:
 	void restoreFlash();
 
 	const uint8_t maxButtons = 64;
-	std::string printModes();
+	std::string printModes(const std::vector<std::string>& names);
 
 	void setMode(SPI_BtnMode mode);
 	void initSPI();
 	const ClassType getClassType() override {return ClassType::Buttonsource;};
+
+	void setSpiSpeed(uint8_t speedPreset);
 
 protected:
 	SPI_Buttons(uint16_t configuration_address, uint16_t configuration_address_2);
@@ -75,6 +79,8 @@ private:
 	ButtonSourceConfig conf;
 
 	uint8_t spi_buf[4] = {0};
+
+	static constexpr std::array<uint32_t,3> speedPresets= {SPI_BAUDRATEPRESCALER_16,SPI_BAUDRATEPRESCALER_32,SPI_BAUDRATEPRESCALER_64};
 };
 
 class SPI_Buttons_1 : public SPI_Buttons {
