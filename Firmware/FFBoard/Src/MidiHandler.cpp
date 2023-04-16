4cba43e135e98ba9b71bde3406d0b3e83645614e
Yannick Richter <git@y-richter.de>
2023-04-16 17:17:33 +0200

    Added midi clock to midihandler

--- Firmware/FFBoard/Src/MidiHandler.cpp
+++ Firmware/FFBoard/Src/MidiHandler.cpp
@@ -28,39 +28,52 @@ void MidiHandler::midiRx(uint8_t itf,uint8_t packet[4]){
 	uint8_t b1 =  packet[2];
 	uint8_t b2 =  packet[3];
 
-	switch (packettype) {
-	case 0x80:
-		// Noteoff
-		this->noteOff(chan, b1, b2);
+	switch(packet[1]){ // System packet
+	case 0xF8:
+		// tick
+		this->midiTick();
 		break;
-	case 0x90:
-		// Noteon
-		if(b2 == 0){
-			this->noteOff(chan, b1, b2);
-		}else{
-			this->noteOn(chan, b1, b2);
-		}
-		break;
-	case 0xB0:
-		//cc
-		this->controlChange(chan, b1,b2);
-		break;
-	case 0xC0:
-		//pc
-		break;
-	case 0xD0:
 
-		break;
-	case 0xE0:
-	{
-		//pb
-		int16_t pb = (b1 & 0x7f) | ((b2 & 0x7f) << 7);
-		this->pitchBend(chan, pb-8192);
-		break;
-	}
 	default:
-		break;
+		// Other packet
+	switch (packettype) {
+		case 0x80:
+			// Noteoff
+			this->noteOff(chan, b1, b2);
+			break;
+		case 0x90:
+			// Noteon
+			if(b2 == 0){
+				this->noteOff(chan, b1, b2);
+			}else{
+				this->noteOn(chan, b1, b2);
+			}
+			break;
+		case 0xB0:
+			//cc
+			this->controlChange(chan, b1,b2);
+			break;
+		case 0xC0:
+			//pc
+			this->programChange(chan,b1);
+			break;
+		case 0xD0:
+
+			break;
+		case 0xE0:
+		{
+			//pb
+			int16_t pb = (b1 & 0x7f) | ((b2 & 0x7f) << 7);
+			this->pitchBend(chan, pb-8192);
+			break;
+		}
+		default:
+			break;
+		}
+	break;
 	}
+
+
 }
 
 void MidiHandler::noteOn(uint8_t chan, uint8_t note,uint8_t velocity){
@@ -73,6 +86,15 @@ void MidiHandler::controlChange(uint8_t chan, uint8_t c, uint8_t val){
 
 }
 void MidiHandler::pitchBend(uint8_t chan, int16_t val){
+
+}
+/**
+ * Sent 24 times per quarter note
+ */
+void MidiHandler::midiTick(){
+
+}
+void MidiHandler::programChange(uint8_t chan, uint8_t val){
 
 }
 
