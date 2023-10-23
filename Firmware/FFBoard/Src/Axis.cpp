6d826d6c844bd61556164b9b3d1b4ce5bdc03ce4
Yannick Richter <git@y-richter.de>
2023-10-23 17:21:41 +0200

    Check axis cpr if tmc has encoder

--- Firmware/FFBoard/Src/Axis.cpp
+++ Firmware/FFBoard/Src/Axis.cpp
@@ -903,7 +903,7 @@ CommandStatus Axis::command(const ParsedCommand& cmd,std::vector<CommandReply>& 
 			uint32_t cpr = this->drv->getEncoder()->getCpr();
 #ifdef TMC4671DRIVER
 			TMC4671 *tmcdrv = dynamic_cast<TMC4671 *>(this->drv.get()); // Special case for TMC. Get the actual encoder resolution
-			if (tmcdrv)
+			if (tmcdrv && tmcdrv->hasIntegratedEncoder())
 			{
 				cpr = tmcdrv->getEncCpr();
 			}
