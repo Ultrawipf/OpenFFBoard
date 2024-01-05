c6ad21b709f0e29a0770f0433a0878f31e831c72
Yannick Richter <git@y-richter.de>
2024-01-05 15:20:01 +0100

    Prevent brake resistor from activating if under a voltage limit
    Would activate resistor sometimes if only usb supply leakage is measured

--- Firmware/FFBoard/Src/voltagesense.cpp
+++ Firmware/FFBoard/Src/voltagesense.cpp
@@ -16,8 +16,10 @@ uint32_t voltageDiffActivate = 5000;
 uint32_t voltageDiffDeactivate = 4000;
 float vSenseMult = VOLTAGE_MULT_DEFAULT;
 bool brake_failure = false;
+uint32_t minVoltage = 6500;
 
 uint32_t brakeActiveTime = 0;
+const Error resError = Error(ErrorCode::brakeResistorFailure, ErrorType::critical, "Brake resistor stuck on");
 
 /*
  * Multiplier to convert ADC counts to a sensed voltage on the vInt and vExt pins.
@@ -53,6 +55,11 @@ void brakeCheck(){
 	}
 	uint16_t vint = getIntV();
 	uint16_t vext = getExtV();
+
+	if(vint < minVoltage && vext < minVoltage){
+		return; // Do not enable if device is unpowered (just measuring usb leakage)
+	}
+
 	bool lastBrakingFlag = braking_flag;
 	braking_flag = (vint > vext + voltageDiffActivate) || vint > maxVoltage || (braking_flag && (vint > vext + voltageDiffDeactivate));
 			//(ADC_BUF[ADC_CHAN_VINT] > ADC_BUF[ADC_CHAN_VEXT]+400 || (ADC_BUF[ADC_CHAN_VINT] > 3000));
@@ -67,7 +74,8 @@ void brakeCheck(){
 		// Brake resistor active over 5s. Shut down.
 		brake_failure = true;
 		HAL_GPIO_WritePin(DRV_BRAKE_GPIO_Port,DRV_BRAKE_Pin, GPIO_PIN_RESET);
-		ErrorHandler::addError(Error(ErrorCode::brakeResistorFailure, ErrorType::critical, "Brake resistor stuck on"));
+		// TODO: can not create error here in ISR. may cause faults
+		ErrorHandler::addError(resError);
 	}else{
 		HAL_GPIO_WritePin(DRV_BRAKE_GPIO_Port,DRV_BRAKE_Pin, braking_flag ? GPIO_PIN_SET:GPIO_PIN_RESET);
 	}
