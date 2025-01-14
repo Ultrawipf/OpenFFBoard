533498e4c98a267f7b572cd1f25f6d150b8b70ea
Yannick Richter <git@y-richter.de>
2025-01-14 13:18:56 +0100

    F407_disco trigger ADC by timer

--- Firmware/Targets/F407VG_DISCO/Core/Src/stm32f4xx_it.c
+++ Firmware/Targets/F407VG_DISCO/Core/Src/stm32f4xx_it.c
@@ -71,7 +71,6 @@ extern DMA_HandleTypeDef hdma_spi3_tx;
 extern SPI_HandleTypeDef hspi3;
 extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim2;
-extern TIM_HandleTypeDef htim4;
 extern TIM_HandleTypeDef htim5;
 extern TIM_HandleTypeDef htim6;
 extern TIM_HandleTypeDef htim9;
@@ -335,20 +334,6 @@ void TIM2_IRQHandler(void)
   /* USER CODE BEGIN TIM2_IRQn 1 */
 
   /* USER CODE END TIM2_IRQn 1 */
-}
-
-/**
-  * @brief This function handles TIM4 global interrupt.
-  */
-void TIM4_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM4_IRQn 0 */
-
-  /* USER CODE END TIM4_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim4);
-  /* USER CODE BEGIN TIM4_IRQn 1 */
-
-  /* USER CODE END TIM4_IRQn 1 */
 }
 
 /**
