e30b71d173672abbfb494b3dbc17dbcdabfe96f5
Yannick Richter <git@y-richter.de>
2026-02-25 09:51:59 +0100

    F407 general set encoder filter to 5

--- Firmware/Targets/F407VG/Core/Src/main.c
+++ Firmware/Targets/F407VG/Core/Src/main.c
@@ -881,7 +881,7 @@ static void MX_TIM3_Init(void)
   sConfig.IC1Polarity = TIM_ICPOLARITY_FALLING;
   sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
-  sConfig.IC1Filter = 0;
+  sConfig.IC1Filter = 5;
   sConfig.IC2Polarity = TIM_ICPOLARITY_FALLING;
   sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
